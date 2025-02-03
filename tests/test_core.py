d6482445a32e73e91b547b3ec7d8f7ed0746b04c
CNelson <colleenfnelson@gmail.com>
2025-02-02 22:41:59 -0700

    Tests update

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,17 +5,31 @@ from singer_sdk.testing import get_tap_test_class
 
 from tap_riotapi.tap import TapRiotAPI
 
-SAMPLE_CONFIG = {
-    "auth_token": "RGAPI-4c29620e-ddbe-4f84-9eb9-06413947a961",
-    "followed_players": ["SupremeKitteh#NA1"],
+SAMPLE_CONFIG_BASE = {
+    "auth_token": "",
+}
+PLAYER_CONFIG = {
+    "followed_players": {"NA": ["SupremeKitteh#NA1"]}
+}
+LEAGUE_CONFIG = {
+    "followed_leagues":{
+        "NA": [
+            {"name": "challenger"},
+            {"name": "diamond"},
+            {"name": "iron", "division": 4},
+        ]
+    }
 }
 
 
 # Run standard built-in tap tests from the SDK:
 TestTapRiotAPI = get_tap_test_class(
     tap_class=TapRiotAPI,
-    config=SAMPLE_CONFIG,
+    config=(SAMPLE_CONFIG_BASE | PLAYER_CONFIG),
 )
 
 
 # TODO: Create additional tests as appropriate for your tap.
+
+# TODO: test for no streams configured
+# TODO: test for invalid API key
