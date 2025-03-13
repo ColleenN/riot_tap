a03243264f38689813573fc25df3ed2235c7dbae
CNelson <colleenfnelson@gmail.com>
2025-03-13 15:51:55 -0600

    Alter test to skip test_tap_stream_returns_record for streams where zero records is valid

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,35 +4,35 @@ from singer_sdk.testing import get_tap_test_class
 
 from tap_riotapi.tap import TapRiotAPI
 
-SAMPLE_CONFIG_BASE = {
-    "auth_token": "RGAPI-704a63d0-704c-4ea0-bd65-092df415bda9",
-}
-PLAYER_CONFIG = {"followed_players": {"NA1": ["SupremeKitteh#NA1"]}}
-LEAGUE_CONFIG = {
-    "followed_leagues": {
-        "NA1": [
-            {"name": "challenger"},
-            # {"name": "diamond"},
-            # {"name": "iron", "division": 4},
-        ]
-    }
-}
+PLAYER_CONFIG = {"players": ["SupremeKitteh#NA1"]}
+# LEAGUE_CONFIG = {"leagues": [{"name": "challenger"}, {"name": "diamond"}, {"name": "iron", "division": 4}]}
 
-
+SAMPLE_CONFIG = {
+    "auth_token": "",
+    "following": {"NA1": PLAYER_CONFIG},
+}
 # Run standard built-in tap tests from the SDK:
-TestTapRiotAPI = get_tap_test_class(
-    tap_class=TapRiotAPI,
-    config=(SAMPLE_CONFIG_BASE | PLAYER_CONFIG),
-)
 
+def build_riot_tap_test_class():
 
-# get_tap_test_class(
-#    tap_class=TapRiotAPI,
-#    config=(SAMPLE_CONFIG_BASE | LEAGUE_CONFIG),
-# )
+    initial = get_tap_test_class(
+        tap_class=TapRiotAPI,
+        config=SAMPLE_CONFIG,
+    )
+    to_keep_names = []
+    to_keep_params = []
+    for name, params in zip(initial.param_ids['test_tap_stream_returns_record'], initial.params['test_tap_stream_returns_record']) :
+        if not ('_match_detail' in name or '_match_history' in name):
+            to_keep_names.append(name)
+            to_keep_params.append(params)
+    initial.param_ids['test_tap_stream_returns_record'] = to_keep_names
+    initial.params['test_tap_stream_returns_record'] = to_keep_params
+
+    return initial
+
+TestTapRiotAPI = build_riot_tap_test_class()
 
 
 # TODO: Create additional tests as appropriate for your tap.
-
 # TODO: test for no streams configured
 # TODO: test for invalid API key
